f2071629661d27aa07367ec4fa55388fe0163c43
Suheyla Cetin <Suheyla@mehmets-MacBook-Pro.local>
2022-04-13 16:23:06 -0700

    Suheyla navbar links

--- components/HamburgerMenu.js
+++ components/HamburgerMenu.js
@@ -27,30 +27,11 @@ export default function HamburgerMenu() {
 					</Offcanvas.Header>
 					<Offcanvas.Body>
 						<Nav className="justify-content-end flex-grow-1 pe-3">
-							<Nav.Link href="#action1">Home</Nav.Link>
-							<Nav.Link href="#action2">Add program</Nav.Link>
-							<Nav.Link href="#action3">View history</Nav.Link>
-							<Nav.Link href="#action4">Logout</Nav.Link>
-							<NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
-								<NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-								<NavDropdown.Item href="#action4">
-									Another action
-								</NavDropdown.Item>
-								<NavDropdown.Divider />
-								<NavDropdown.Item href="#action5">
-									Something else here
-								</NavDropdown.Item>
-							</NavDropdown>
+							<Nav.Link href="/">Home</Nav.Link>
+							<Nav.Link href="/CreateWorkout">Add program</Nav.Link>
+							<Nav.Link href="/WorkoutHistory">View history</Nav.Link>
+							<Nav.Link href="/users/login">Logout</Nav.Link>
 						</Nav>
-						<Form className="d-flex">
-							<FormControl
-								type="search"
-								placeholder="Search"
-								className="me-2"
-								aria-label="Search"
-							/>
-							<Button variant="outline-success">Search</Button>
-						</Form>
 					</Offcanvas.Body>
 				</Navbar.Offcanvas>
 			</Container>
