33569dc9ff7e5b31b98ac9b57fdb4a59d8f54a94
Suheyla Cetin <Suheyla@mehmets-MacBook-Pro.local>
2022-04-17 12:11:49 -0700

    Add grid on click of add exercise button

--- pages/CreateWorkout.js
+++ pages/CreateWorkout.js
@@ -1,28 +1,35 @@
 import HamburgerMenu from "../components/HamburgerMenu";
 import React, { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Container } from "react-bootstrap";
 import ExerciseList from "../components/ExerciseList";
+import WorkoutProgram from "../components/WorkoutProgram";
 
+export default function CreateWorkout() {
+	const onInput = (event) => {
+		console.log(event.target.value);
+	};
 
-export default function CreateWorkout() { 
-  const onInput = (event) =>{
-    console.log(event.target.value)
-  } 
-
-  return (
-    <>
-      <HamburgerMenu />
-      <Form>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Control type="email" placeholder="Name Workout" onChange={onInput} />
-        </Form.Group>
-      </Form>
-      <ExerciseList />
-      <Button variant="warning">Cancel</Button>{" "}
-      <Button href="/" variant="success">
-        Save
-      </Button>{" "}
-    </>
-  );
+	return (
+		<>
+			<HamburgerMenu />
+			<Container>
+				<Form>
+					<Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+						<Form.Control
+							type="email"
+							placeholder="Name Workout"
+							onChange={onInput}
+						/>
+					</Form.Group>
+				</Form>
+				<WorkoutProgram />
+				<ExerciseList />
+				<Button variant="warning">Cancel</Button>{" "}
+				<Button href="/" variant="success">
+					Save
+				</Button>{" "}
+			</Container>
+		</>
+	);
 }
