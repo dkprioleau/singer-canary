b5fa8a113f1a119d4f8cb4996a985891d119bece
dkprioleau <dkprioleau@gmail.com>
2022-04-27 13:55:30 -0400

    Danielle_ExerciseList

--- components/ExerciseList.js
+++ components/ExerciseList.js
@@ -2,22 +2,20 @@ import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col, ListGroup, Button } from "react-bootstrap";
 
-export default function ExerciseList({onAddToWorkout}) {
-
+export default function ExerciseList({ onAddToWorkout }) {
   const exampleExercise = {
-    name : "e1",
-    sets : 5,
-    reps : 8,
-    time : 10,
-    weight: 15
-  }
-  
-  function handleAddToWorkout(event){
+    name: "e1",
+    sets: 5,
+    reps: 8,
+    time: 10,
+    weight: 15,
+  };
+
+  function handleAddToWorkout(event) {
     onAddToWorkout(exampleExercise);
   }
 
   const [exercises, setExercises] = useState([exampleExercise]);
-
 
   useEffect(() => {
     const options = {
@@ -33,18 +31,13 @@ export default function ExerciseList({onAddToWorkout}) {
       .then((response) => setExercises(response))
       // can only display in then because that is the only place where we have access to response
       .catch((err) => console.error(err));
-      
   }, []);
-console.log(exercises)
+  console.log(exercises);
   return (
     <>
-    <div>
-      {exercises.length}
-    </div>
+      <div>{exercises.length}</div>
       {exercises.map((exercise) => {
-        return(
-          <div key={exercise.id}>{exercise.name}</div>
-        )
+        return <div key={exercise.id}>{exercise.name}</div>;
 
         // <Container>
         //   <h3>List of Exercises</h3>
@@ -60,7 +53,7 @@ console.log(exercises)
         //     </Col>
         //   </Row>
         // </Container>
-       })} 
+      })}
     </>
   );
 }
