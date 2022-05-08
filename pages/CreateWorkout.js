7fe4984a0ba1c172a29bd6571a31e432c9c9510e
unknown <alicelee.0418@gmail.com>
2022-05-07 17:48:38 -0700

    coding session 5.7

--- pages/CreateWorkout.js
+++ pages/CreateWorkout.js
@@ -5,60 +5,66 @@ import { Form, Button, Container } from "react-bootstrap";
 import ExerciseList from "../components/ExerciseList";
 import ViewWorkoutProgram from "../components/ViewWorkoutProgram";
 
-// parent component
-
 export default function CreateWorkout() {
-  const [isClicked, setIsClicked] = useState(false);
+	const [workout, setWorkout] = useState([]);
+	function newExercise(exercise) {
+		console.log(exercise);
+		setWorkout([...workout, exercise]);
+	}
 
-  const [workout, setWorkout] = useState([]);
-  function newExercise(exercise) {
-    console.log(exercise);
-    setWorkout([...workout, exercise]);
-  }
+	const editWorkout = (value, index, property) => {
+		workout[index][property] = value;
+		setWorkout([...workout]);
+	};
 
-  const editWorkout = (value, index, property) => {
-    workout[index][property] = value;
-    setWorkout([...workout]);
-  };
+	const save = () => {
+		console.log(workout);
+		console.log("before adding");
+		db.collection("workout").add({
+			exercises: [{
+				id: "002",
+				name: "push up",
+				reps: 2,
+				sets: 3,
+				time: 3,
+				weight:5
+		}],
+			id: 2000,
+			name: "Example2"
 
-  const save = () => {
-    console.log(workout);
-    fire.firestore().collection("workout").add({
-      workout: workout,
-    });
-  };
+		}).then(() => {
+			console.log("Document successfully written!");
+		})
+		.catch((error) => {
+			console.error("Error writing document: ", error);
+		});
+		console.log("after adding to firestore");
+	};
 
-  const onInput = (event) => {
-    console.log(event.target.value);
-  };
+	const onInput = (event) => {//workout has two objects, name(string), exercises(arrays)
+		console.log(event.target.value);
+	};
 
-  return (
-    <>
-      <HamburgerMenu />
-      <Container>
-        <Form>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Control
-              type="email"
-              placeholder="Name Workout"
-              onChange={onInput}
-            />
-          </Form.Group>
-        </Form>
-        <ViewWorkoutProgram workout={workout} editWorkout={editWorkout} />
-        <Button onClick={() => setIsClicked(!isClicked)}>Add Exercise</Button>
-        {/* onclick shows list of exercises */}
-
-        {isClicked && (
-          <div>
-            <ExerciseList onAddToWorkout={newExercise} />
-            <Button href="/"variant="warning">Cancel</Button>{" "}
-            <Button variant="success" onClick={save}>
-              Save
-            </Button>{" "}
-          </div>
-        )}
-      </Container>
-    </>
-  );
+	return (
+		<>
+			<HamburgerMenu />
+			<Container>
+				<Form>
+					<Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+						<Form.Control
+							type="email"
+							placeholder="Name Workout"
+							onChange={onInput}
+						/>
+					</Form.Group>
+				</Form>
+				<ViewWorkoutProgram workout={workout} editWorkout={editWorkout} />
+				<ExerciseList onAddToWorkout={newExercise} />
+				<Button variant="warning">Cancel</Button>{" "}
+				<Button variant="success" onClick={save}>
+					Save
+				</Button>{" "}
+			</Container>
+		</>
+	);
 }
